1f25ec378aa35e1ec5883c2ab24e3ea7063e9e42
Michael Peter <michaelhpet@gmail.com>
2023-06-27 22:06:41 +0100

    feat: insert node at left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_tree.h"
+
+/**
+* binary_tree_insert_left - insert a node ad the left
+* child of another node
+* @parent: pointer to parent node
+* @value: value of node to insert
+* Return: pointer to created node, NULL if failed
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node, *prev_left;
+
+	if (parent == NULL)
+		return (NULL);
+
+	prev_left = parent->left;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
+	parent->left = node;
+	node->parent = parent;
+	node->value = value;
+	node->left = prev_left;
+	if (prev_left)
+		prev_left->parent = node;
+	node->right = NULL;
+
+	return (node);
+}
