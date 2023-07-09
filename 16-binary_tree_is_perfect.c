f445346e1c260e78410bd77876240b6184382443
Michael Peter <michaelhpet@gmail.com>
2023-07-09 20:57:19 +0100

    feat: define binary_tree_is_perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+
+/**
+* binary_tree_is_perfect - Check if a binary tree is perfect
+* @tree: Pointer to binary tree
+* Return: 1 if tree is perfect, 0 otherwise
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+}
